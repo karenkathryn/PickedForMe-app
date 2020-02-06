97663f509ad8ad7892d8571e7f0fdb7d2e71379a
karenkathryn <karenwarmbein@gmail.com>
2020-02-06 11:19:57 -0800

    work on css; color gradient working

--- src/models/recipe_predictor.py
+++ src/models/recipe_predictor.py
@@ -28,11 +28,12 @@ def main_function(test_pos_value, test_neg_value):
 def main_function_2(my_food_list):
     """This is the function that predicts 5 or less recipes."""
     choice = len(my_food_list)
-
+    print(choice)
     file_tfidf = "src/models/model-tfidf.pkl"
     infile = open(file_tfidf,'rb')
     df_tfidf = pk.load(infile)
     #infile.close()
+
 
     if choice == 1:    
         # USER -- Choose 1 result
@@ -44,12 +45,12 @@ def main_function_2(my_food_list):
         recipe_dict = dict(list(enumerate(recipe_list)))
         return recipe_dict
 
-    else:
+    elif choice == 2:
         # USER -- Choose 2 results   
         my_recipe_list = df_tfidf[(df_tfidf[my_food_list[0]] > 0) & 
                                     (df_tfidf[my_food_list[1]] > 0)
                                     ].index.tolist()  
-        
+        print("in the elif")
         if len(my_recipe_list) > 0:
             recipe_list = []
             for item in my_recipe_list:
@@ -60,7 +61,11 @@ def main_function_2(my_food_list):
             recipe_dict = dict(list(enumerate(recipe_list)))
             return recipe_dict
         else: 
+            print("in the elif else")
             return {100: "There are no recipies with these ingredients."}
+
+    else:
+        return {50: "That is not a valid option."}
 
 ############################################################
 # Helper functions begin below #
@@ -123,30 +128,30 @@ def length_recipe_list(my_recipe_list):
 #         #       print(f'https://www.food.com/recipe/{my_recipe_list[i]}')
 
 #     else:
-#         # Case: there are no recipes with the 2 chosen ingredients, so find something similar
-#         print(f'There are no recipes with this ingredient set. Here are some suggestions:')
-#         my_food_2 = result[2][0]
-#         my_recipe_list = df_tfidf[df_tfidf[my_food_2] > 0].index.tolist()
-#         list_secondary = []
-#         for id_ in my_recipe_list:
-#             # use the tf-idf cosine similarity to find something similar
-#             for column_id in df_similarity.columns:     
-#                 # check if the similarity is between values   
-#                 if df_similarity.loc[id_,  column_id] > .01:
-#                     value = df_similarity.loc[id_,  column_id]
-#                     list_secondary.append([column_id, value])
+        # # Case: there are no recipes with the 2 chosen ingredients, so find something similar
+        # print(f'There are no recipes with this ingredient set. Here are some suggestions:')
+        # my_food_2 = result[2][0]
+        # my_recipe_list = df_tfidf[df_tfidf[my_food_2] > 0].index.tolist()
+        # list_secondary = []
+        # for id_ in my_recipe_list:
+        #     # use the tf-idf cosine similarity to find something similar
+        #     for column_id in df_similarity.columns:     
+        #         # check if the similarity is between values   
+        #         if df_similarity.loc[id_,  column_id] > .01:
+        #             value = df_similarity.loc[id_,  column_id]
+        #             list_secondary.append([column_id, value])
                 
-#             pri_sec_values_df = pd.DataFrame(list_secondary, columns=['secondary', 'cs_value'])
-#             # sort by cosine similarity value 
-#             pri_sec_values_df = pri_sec_values_df.sort_values('cs_value')
-#             if len(pri_sec_values_df) > 4:
-#                 pri_sec_values_df = pri_sec_values_df.tail(5)
-#             recipe_list = []
-#             for i in range(5):
-#                 sec_id = int(pri_sec_values_df.iloc[i]['secondary'])
-#                 recipe_list.append(f'https://www.food.com/recipe/{sec_id}')
+        #     pri_sec_values_df = pd.DataFrame(list_secondary, columns=['secondary', 'cs_value'])
+        #     # sort by cosine similarity value 
+        #     pri_sec_values_df = pri_sec_values_df.sort_values('cs_value')
+        #     if len(pri_sec_values_df) > 4:
+        #         pri_sec_values_df = pri_sec_values_df.tail(5)
+        #     recipe_list = []
+        #     for i in range(5):
+        #         sec_id = int(pri_sec_values_df.iloc[i]['secondary'])
+        #         recipe_list.append(f'https://www.food.com/recipe/{sec_id}')
         
-#             return recipe_list
+        #     return recipe_list
 
 
 
