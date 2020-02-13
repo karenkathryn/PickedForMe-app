79e4f69ac842a5eab96883e2918bbe7a054a6f41
karenkathryn <karenwarmbein@gmail.com>
2020-02-13 11:02:41 -0800

    fix bug - list similar recipes that are listed as primary; update wording

--- src/models/recipe_predictor.py
+++ src/models/recipe_predictor.py
@@ -34,17 +34,27 @@ def main_function_2(my_food_list):
 
     choice = len(my_food_list)
 
+    file_df = "src/models/recipe_data_220k.pkl"
+    df = load_model(file_df)
+    df = df.set_index('id')
+    df['name'] = df.iloc[:, 0].str.replace(r'\ {2,}', " ", regex=True).str.replace(" s ", "'s ").str.upper()
+
     # USER -- Choose 1 result
-    if choice == 1:    
+    if choice == 1: 
+       
         my_recipe_list = df_tfidf[(df_tfidf[my_food_list[0]] > 0)].index.tolist()  
         recipe_list = []
+        recipe_name = []
         my_range_value = length_recipe_list(my_recipe_list)
         for i in range(my_range_value):
-            recipe_list.append('https://www.food.com/recipe/' + str(my_recipe_list[i]))
+            recipe_id = my_recipe_list[i]
+            recipe_list.append('https://www.food.com/recipe/' + str(recipe_id))
+            recipe_name.append( df.loc[recipe_id, 'name'] )
+
         recipe_dict = dict(list(enumerate(recipe_list)))
-        
-        sim_recipe_dict = main_function_3(my_recipe_list)
-        return '1', recipe_dict, sim_recipe_dict
+        recipe_name = dict(list(enumerate(recipe_name)))
+        sim_recipe_dict, sim_name_dict = main_function_3(df, my_recipe_list)
+        return '1', recipe_dict, sim_recipe_dict, recipe_name, sim_name_dict
 
     # USER -- Choose 2 results
     elif choice == 2:   
@@ -62,21 +72,26 @@ def main_function_2(my_food_list):
             return '2a', recipe_dict, ''
         else: 
             # try individually
+            
+            recipe_name = []
             recipe_list = []
             for item in my_food_list:
                 my_recipe_list = df_tfidf[df_tfidf[item] > 0].index.tolist()  
                 my_range_value = length_recipe_list(my_recipe_list)
                 for i in range(my_range_value):
-                    recipe_list.append('https://www.food.com/recipe/' + str(my_recipe_list[i]))
+                    recipe_id = my_recipe_list[i]
+                    recipe_list.append('https://www.food.com/recipe/' + str(recipe_id))
+                    recipe_name.append( df.loc[recipe_id, 'name'] )
             recipe_dict = dict(list(enumerate(recipe_list)))
-            return '2b', recipe_dict, 'There are no recipes with this choice of ingredients. Here are some suggestions:'
+            name_dict = dict(list(enumerate(recipe_name)))
+            return '2b', recipe_dict, 'There are no recipes with this choice of ingredients. Here are some suggestions:', name_dict
 
     else:
         # USER -- Choose 3+ results
         return '3', {100: "That is not a valid option."}, ''
 
-def main_function_3(my_recipe_list):
-    """ """
+def main_function_3(df, my_recipe_list):
+    """The function that finds similar recipes """
     file_csm = "src/models/model-csm.pkl"
     df_similarity = load_model(file_csm)
     
@@ -86,9 +101,10 @@ def main_function_3(my_recipe_list):
         for column_id in df_similarity.columns:        
             if df_similarity.loc[id_,  column_id] > 0:
                 value = df_similarity.loc[id_,  column_id]
-                if value not in my_recipe_list:
+                if column_id not in my_recipe_list:
                     list_secondary.append([column_id, value])
-            
+                
+
         pri_sec_values_df = pd.DataFrame(list_secondary, columns=['secondary', 'cs_value'])
         # sort by cosine similarity value 
         pri_sec_values_df = pri_sec_values_df.sort_values('cs_value')
@@ -97,12 +113,16 @@ def main_function_3(my_recipe_list):
             pri_sec_values_df = pri_sec_values_df.tail(5)
         
         recipe_list = []
+        recipe_name = []
         for i in range(5):
             sec_id = int(pri_sec_values_df.iloc[i]['secondary'])
             recipe_list.append('https://www.food.com/recipe/' + str(sec_id))
+            recipe_name.append( df.loc[sec_id, 'name'] )
+
         print(recipe_list)
         recipe_dict = dict(list(enumerate(recipe_list)))
-        return recipe_dict
+        name_dict = dict(list(enumerate(recipe_name)))
+        return recipe_dict, name_dict
 
 
 
